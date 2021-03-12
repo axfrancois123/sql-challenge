96796f9d35dfea879111a809d4b64dcb44a54db3
Armand Francois <armandfrancois56@gmail.com>
2021-03-11 20:51:44 -0600

    Last Push

---
+++ EmployeeSQL/Schema - SQL_Challenge.sql
@@ -0,0 +1,68 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" varchar(30)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL,
+    PRIMARY KEY ("dept_no")
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(30)   NOT NULL,
+    PRIMARY KEY ("emp_no","dept_no"),
+	FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no"),
+	FOREIGN KEY("dept_no") REFERENCES "departments" ("dept_no")	
+);
+
+CREATE TABLE "dept_manager" (
+	"dept_no" varchar(30)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    PRIMARY KEY ("emp_no"),
+	FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no"),
+	FOREIGN KEY("dept_no") REFERENCES "departments" ("dept_no")
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(30)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "sex" varchar(4)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    PRIMARY KEY ("emp_no"),
+	FOREIGN KEY("emp_title_id") REFERENCES "titles" ("title_id")
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    PRIMARY KEY ("emp_no"),
+	FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no")
+);
+
+CREATE TABLE "titles" (
+    "title_id" varchar(30)   NOT NULL,
+    "title" varchar(30)   NOT NULL,
+    PRIMARY KEY ("title_id")
+);
+
+-- ALTER TABLE "dept_emp" FOREIGN KEY("emp_no")
+-- REFERENCES "employees" ("emp_no");
+
+-- ALTER TABLE "dept_emp" FOREIGN KEY("dept_no")
+-- REFERENCES "departments" ("dept_no");
+
+-- ALTER TABLE "dept_manager" FOREIGN KEY("emp_no")
+-- REFERENCES "employees" ("emp_no");
+
+-- ALTER TABLE "dept_manager" FOREIGN KEY("dept_no")
+-- REFERENCES "departments" ("dept_no");
+
+-- ALTER TABLE "employees" FOREIGN KEY("emp_title_id")
+-- REFERENCES "titles" ("title_id");
+
+-- ALTER TABLE "salaries" FOREIGN KEY("emp_no")
+-- REFERENCES "employees" ("emp_no");
